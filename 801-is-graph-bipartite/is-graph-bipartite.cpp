fc66b5f4a2a30d7bdab8bdc5cac247b6141b4815
Devansh Sharma <122260173+devsansh@users.noreply.github.com>
2024-03-06 22:47:12 +0530

    Time: 19 ms (50.68%) | Memory: 16.7 MB (15.39%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,29 @@
+enum class Color { kWhite, kRed, kGreen };
+
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<Color> colors(graph.size(), Color::kWhite);
+
+        for (int i = 0; i < graph.size(); ++i) {
+            if (colors[i] != Color::kWhite)
+                continue;
+            colors[i] = Color::kRed;
+            queue<int> q{{i}};
+            while (!q.empty()) {
+                const int u = q.front();
+                q.pop();
+                for (const int v : graph[u]) {
+                    if (colors[v] == colors[u])
+                        return false;
+                    if (colors[v] == Color::kWhite) {
+                        colors[v] = colors[u] == Color::kRed ? Color::kGreen : Color::kRed;
+                        q.push(v);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
